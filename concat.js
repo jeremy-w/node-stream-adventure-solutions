2ed4e0b12add502a21a9358d2215e329adeba8af
Wagner Silveira <wagnerhsilveira@gmail.com>
2015-03-29 23:20:36 -0300

    Update concat.js
    var through = require('through')
    Through module is not required to get the solution

--- concat.js
+++ concat.js
@@ -1,5 +1,4 @@
 var concat = require('concat-stream')
-var through = require('through')
 
 process.stdin.pipe(concat(function (buffer) {
   process.stdout.write(buffer.toString().split('').reverse().join(''))
